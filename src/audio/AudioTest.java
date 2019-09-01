93f705813194b36aaf6fdc44ddd9a8324aae3897
RobotOnFire <robotronfire@gmail.com>
2019-09-01 12:16:25 -0700

    Added 3D sound

--- src/audio/AudioTest.java
+++ src/audio/AudioTest.java
@@ -1,8 +1,10 @@
 package audio;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Keyboard;
 import org.lwjgl.openal.AL;
 import org.lwjgl.openal.AL10;
 import org.lwjgl.openal.AL11;
@@ -13,7 +15,7 @@ public class AudioTest {
 	static int buffer;
 	static Source source;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException{
 		try {
 			AL.create();
 			AL11.alListener3i(AL10.AL_POSITION, 0, 0, 0);
@@ -21,9 +23,21 @@ public class AudioTest {
 		} catch (LWJGLException e) {
 			e.printStackTrace();
 		}
-		buffer = loadSound("audio/bruh_audio5.wav");
+		buffer = loadSound("audio/bruh_audio6.wav");
 		source = new Source(); 
+		source.setLooping(true);
 		source.play(buffer);
+		
+		float xPos = 2;
+		source.setPosition(xPos, 0, 0);
+		while(xPos>-10) {
+			xPos-=0.03f;
+			source.setPosition(xPos, 0, 0);
+			Thread.sleep(10);
+		}
+		source.delete();
+		AL10.alDeleteBuffers(buffer);
+		AL.destroy();
 	}
 	
 	static int loadSound(String fileName) {
--- src/audio/Source.java
+++ src/audio/Source.java
@@ -1,22 +1,36 @@
 package audio;
 
 import org.lwjgl.openal.AL10;
+import org.lwjgl.openal.AL11;
 
 public class Source {
 
 	int sourceID;
-	
+
 	public Source() {
 		sourceID = AL10.alGenSources();
 	}
-	
+
+	public void setPosition(float x, float y, float z) {
+		AL10.alSource3f(sourceID, AL10.AL_POSITION, x, y, z);
+	}
+
+	public void setVelocity(float x, float y, float z) {
+		AL10.alSource3f(sourceID, AL10.AL_VELOCITY, x, y, z);
+	}
+
 	public void setLooping(boolean looping) {
 		AL10.alSourcei(sourceID, AL10.AL_LOOPING, looping ? AL10.AL_TRUE : AL10.AL_FALSE);
 	}
-	
+
 	public void play(int buffer) {
+		AL10.alSourceStop(sourceID);
 		AL10.alSourcei(sourceID, AL10.AL_BUFFER, buffer);
 		AL10.alSourcePlay(sourceID);
+	}
+
+	public void delete() {
+		AL10.alDeleteSources(sourceID);
 	}
 	
 }
